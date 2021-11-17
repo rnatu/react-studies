60d16ad45dff6dc11d18316cba8a3f029df06dba
Renato Xavier <rnatu91@gmail.com>
2021-11-17 15:58:40 -0300

    feat: add useNumber and useIsMounted

--- custom-hooks-with-typescript/src/App.tsx
+++ custom-hooks-with-typescript/src/App.tsx
@@ -1,8 +1,25 @@
+import { useEffect } from "react";
+import { useNumber } from "./hooks/01-useNumber";
+import { useIsMounted } from "./hooks/02-useIsMounted";
+
 function App() {
+  const [count, setCount] = useNumber(1);
+  const isMounted = useIsMounted();
+
+  useEffect(() => {
+    if (isMounted) {
+      console.log("component montado");
+    }
+  }, [isMounted]);
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <button
+        type="button"
+        onClick={() => setCount((oldState) => oldState + 1)}
+      >
+        count is {count}
+      </button>
     </div>
   );
 }
