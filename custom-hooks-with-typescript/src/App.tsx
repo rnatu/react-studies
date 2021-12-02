0c6a9a1afcf8f97f320ba3ef6829ab548c493d83
Renato Xavier <rnatu91@gmail.com>
2021-12-02 14:01:58 -0300

    feat: add useDebouncedValue

--- custom-hooks-with-typescript/src/App.tsx
+++ custom-hooks-with-typescript/src/App.tsx
@@ -1,13 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ToggleButton } from "./components/ToggleButton";
 import { useNumber } from "./hooks/01-useNumber";
 import { useIsMounted } from "./hooks/02-useIsMounted";
 import { useToggle } from "./hooks/03-useToggle";
+import { useDebouncedValue } from "./hooks/04-useDebouncedValue";
 
 function App() {
   const [count, setCount] = useNumber(1);
   const isMounted = useIsMounted();
   const [isActive, toggleIsActive] = useToggle(false);
+  const [search, setSearch] = useState("");
+
+  const debouncedValue = useDebouncedValue(search, 1000);
 
   console.log("toggle isActive " + isActive);
 
@@ -19,17 +23,25 @@ function App() {
 
   return (
     <div className="App">
+      
       <button
         type="button"
         onClick={() => setCount((oldState) => oldState + 1)}
       >
         count is {count}
       </button>
-
       <br />
       <br />
-
+      {/* useToggle */}
       <ToggleButton toggleIsActive={toggleIsActive} />
+      <br />
+      <br />
+      {/* useDebouncedValue */}
+      <span>Search </span>
+      <input type="text" onChange={(e) => setSearch(e.target.value)} />{" "}
+      <button>Buscar</button>
+      <br />
+      <a href="/">{debouncedValue}</a>
     </div>
   );
 }
